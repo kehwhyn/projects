1cd5b873b21379b1fdf5e8aa90798f65a342da0d
kehwhyn <kevinboucinha@gmail.com>
2019-08-13 16:54:39 -0300

    atualizando

--- C/diagonal.c
+++ C/diagonal.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int diagonalDifference(int arr_rows, int arr_columns, int** arr) {
-
-    int dp = 0, ds = 0;
-
-    for(int j = arr_columns-1, i = 0; j >= 0 && i < arr_rows; j--, i++)
-        ds += arr[i][j];
-	    dp += arr[i][i];
-
-    return abs(dp - ds);
-}
-
 int main(void){
 
+    int const row = 3, col = 3;
+    int **matrix = calloc(row, sizeof(int*));
 
-    
+    printf("%zu\n%zu\n%zu\n", sizeof(int**),sizeof(int*),sizeof(int));
     return 0;
 }
